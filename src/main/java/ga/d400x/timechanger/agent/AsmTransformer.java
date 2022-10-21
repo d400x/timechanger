987b41da7cc6e10645a941b6bd87721c5571c84d
d400x <desperado400x@gmail.com>
2022-10-21 17:30:58 +0900

    (#2) applying offset by ASM methodVisitor

--- src/main/java/ga/d400x/timechanger/agent/AsmTransformer.java
+++ src/main/java/ga/d400x/timechanger/agent/AsmTransformer.java
@@ -1,5 +1,7 @@
 package ga.d400x.timechanger.agent;
 
+import static org.objectweb.asm.Opcodes.*;
+
 import java.lang.instrument.ClassFileTransformer;
 import java.lang.instrument.IllegalClassFormatException;
 import java.security.ProtectionDomain;
@@ -7,17 +9,12 @@ import java.security.ProtectionDomain;
 import org.objectweb.asm.ClassReader;
 import org.objectweb.asm.ClassVisitor;
 import org.objectweb.asm.ClassWriter;
+import org.objectweb.asm.Label;
 import org.objectweb.asm.MethodVisitor;
-import org.objectweb.asm.Opcodes;
-
 
 public class AsmTransformer implements ClassFileTransformer {
 
-//	static final Logger logger = Logger.getLogger(AsmTransformer.class.getCanonicalName());
-
-	static final int ASM_VER = Opcodes.ASM9;
-
-	static final String REPLACE_OWNER = "ga/d400x/timechanger/util/TimeChangerUtil";
+	static final int ASM_VER = ASM9;
 
 	@Override
 	public byte[] transform(ClassLoader loader, String className, Class<?> classBeingRedefined,
@@ -35,13 +32,18 @@ public class AsmTransformer implements ClassFileTransformer {
 			}
 
 			ClassReader classReader = new ClassReader(classfileBuffer);
-			ClassWriter classWriter = new ClassWriter(classReader, 0);
+			//ClassWriter classWriter = new ClassWriter(classReader, ClassWriter.COMPUTE_MAXS | ClassWriter.COMPUTE_FRAMES);
+			ClassWriter classWriter = new ClassWriter(classReader, ClassWriter.COMPUTE_MAXS);	// COMPUTE_FRAMES caused java.lang.ClassCircularityError
 
-			CustomClassVisitor customClassVisitor = new CustomClassVisitor(ASM_VER, classWriter, className);
+			TimeOffsetClassAdapter timeOffsetClassAdapter = new TimeOffsetClassAdapter(ASM_VER, classWriter, className);
+			ClassVisitor cv = timeOffsetClassAdapter;
 
-			classReader.accept(customClassVisitor, 0);
+			//// for develop and debug
+			//cv = new CheckClassAdapter(timeOffsetClassAdapter);
 
-			return customClassVisitor.hasTarget ? classWriter.toByteArray() : null;
+			classReader.accept(cv, ClassReader.EXPAND_FRAMES);
+
+			return timeOffsetClassAdapter.hasTarget ? classWriter.toByteArray() : null;
 		} catch(Throwable t) {
 			System.err.println(t.toString());
 			t.printStackTrace(System.err);
@@ -51,13 +53,13 @@ public class AsmTransformer implements ClassFileTransformer {
 		}
 	}
 
-	private static final class CustomClassVisitor extends ClassVisitor {
+	private static final class TimeOffsetClassAdapter extends ClassVisitor {
 		/**
 		 * @param api {@link ClassVisitor#api}
 		 * @param classVisitor {@link ClassVisitor#cv}
 		 * @param className (for logging)
 		 */
-		protected CustomClassVisitor(int api, ClassVisitor classVisitor, String className) {
+		protected TimeOffsetClassAdapter(int api, ClassVisitor classVisitor, String className) {
 			super(api, classVisitor);
 			this.hasTarget = false;
 			this.className = className;
@@ -76,7 +78,7 @@ public class AsmTransformer implements ClassFileTransformer {
 		@Override
 		public MethodVisitor visitMethod(int access, String name, String descriptor, String signature, String[] exceptions) {
 			if(cv != null) {
-				return new CustomMethodVisitor(ASM_VER, cv.visitMethod(access, name, descriptor, signature, exceptions), this, name);
+				return new TimeOffsetMethodAdapter(ASM_VER, cv.visitMethod(access, name, descriptor, signature, exceptions), this, name);
 			}
 			return null;
 		}
@@ -89,7 +91,7 @@ public class AsmTransformer implements ClassFileTransformer {
 	 * <li>{@link jdk.internal.misc.VM#getNanoTimeAdjustment(long)} (Java9+)</li>
 	 * </ul>
 	 */
-	private static final class CustomMethodVisitor extends MethodVisitor {
+	private static final class TimeOffsetMethodAdapter extends MethodVisitor {
 
 		/**
 		 * @param api {@link MethodVisitor#api}
@@ -97,44 +99,124 @@ public class AsmTransformer implements ClassFileTransformer {
 		 * @param customClassVisitor
 		 * @param methodName target method name (for logging)
 		 */
-		protected CustomMethodVisitor(int api, MethodVisitor methodVisitor, CustomClassVisitor customClassVisitor, String methodName) {
+		protected TimeOffsetMethodAdapter(int api, MethodVisitor methodVisitor, TimeOffsetClassAdapter customClassVisitor, String methodName) {
 			super(api, methodVisitor);
 			this.ccv = customClassVisitor;
 			this.methodName = methodName;
 		}
 
-		CustomClassVisitor ccv;
+		TimeOffsetClassAdapter ccv;
 
 		String methodName;
 
+		/**
+		 * create instructions to get offset-applied System.currentTimeMillis()<br>
+		 * <code>Math.addExact(System.currentTimeMillis(), Long.valueOf(System.getProperty(TimeChangerAgent.PROP_OFFSET, "0")).longValue());</code>
+		 */
+		private void createOffsetTimeMillisCode() {
+			// System.currentTimeMillis
+			super.visitMethodInsn(INVOKESTATIC, "java/lang/System", "currentTimeMillis", "()J", false);
+			// System.getProperty(TimeChangerAgent.PROP_OFFSET, "0")
+			visitLdcInsn(TimeChangerAgent.PROP_OFFSET);
+			visitLdcInsn("0");
+			super.visitMethodInsn(INVOKESTATIC, "java/lang/System", "getProperty", "(Ljava/lang/String;Ljava/lang/String;)Ljava/lang/String;", false);
+			// Long::valueOf
+			super.visitMethodInsn(INVOKESTATIC, "java/lang/Long", "valueOf", "(Ljava/lang/String;)Ljava/lang/Long;", false);
+			// Long::longValue
+			super.visitMethodInsn(INVOKEVIRTUAL, "java/lang/Long", "longValue", "()J", false);
+			// Math.addExact
+			super.visitMethodInsn(INVOKESTATIC, "java/lang/Math", "addExact", "(JJ)J", false);
+		}
+
+		/**
+		 * For debug use: System.out.println(long_val_on_top_of_op-stack);
+		 */
+		@SuppressWarnings("unused")
+		private void printLongValOnOpStackTop() {
+			visitInsn(DUP2);
+			visitFieldInsn(GETSTATIC, "java/lang/System", "out", "Ljava/io/PrintStream;");
+			visitInsn(DUP_X2);
+			visitInsn(POP);
+			super.visitMethodInsn(INVOKEVIRTUAL, "java/io/PrintStream", "println", "(J)V", false);
+		}
+
 		@Override
 		public void visitMethodInsn(int opcode, String owner, String name, String descriptor, boolean isInterface) {
-			if(opcode == Opcodes.INVOKESTATIC && !isInterface)
+			if(opcode == INVOKESTATIC
+					&& !isInterface
+					&& "java/lang/System".equals(owner)
+					&& "currentTimeMillis".equals(name)
+					&& "()J".equals(descriptor))
 			{
-				if("java/lang/System".equals(owner) && "currentTimeMillis".equals(name) && "()J".equals(descriptor)) {
-					// replace System#currentTimeMillis()
-					TimeChangerAgent.log("(" + ccv.className + "#" + methodName + ") target: " + owner + " " + name + " " + descriptor + " opcode=" + opcode + ", isInterface=" + isInterface);
-					owner = REPLACE_OWNER;
-					name = "offsetCurrentTimeMillis";
-					ccv.hasTarget = true;
-				}
-//				else if("java/lang/System".equals(owner) && "nanoTime".equals(name) && "()J".equals(descriptor)) {
-//					// replace System#nanoTime()
-//					TimeChangerAgent.log("(" + ccv.className + "#" + methodName + ") target: " + owner + " " + name + " " + descriptor + " opcode=" + opcode + ", isInterface=" + isInterface);
-//					owner = REPLACE_OWNER;
-//					name = "offsetNanoTime";
-//					ccv.hasTarget = true;
-//				}
-				else if("jdk/internal/misc/VM".equals(owner) && "getNanoTimeAdjustment".equals(name) && "(J)J".equals(descriptor)) {
-					// replace VM#getNanoTimeAdjustment(long)
-					TimeChangerAgent.log("(" + ccv.className + "#" + methodName + ") target: " + owner + " " + name + " " + descriptor + " opcode=" + opcode + ", isInterface=" + isInterface);
-					owner = REPLACE_OWNER;
-					name = "offsetGetNanoTimeAdjustment";
-					ccv.hasTarget = true;
-				}
+				// replace System#currentTimeMillis()
+				TimeChangerAgent.log("(" + ccv.className + "#" + methodName + ") target: " + owner + " " + name + " " + descriptor + " opcode=" + opcode + ", isInterface=" + isInterface);
+				createOffsetTimeMillisCode();
+				ccv.hasTarget = true;
 			}
-			super.visitMethodInsn(opcode, owner, name, descriptor, isInterface);
+			else if(opcode == INVOKESTATIC
+					&& !isInterface
+					&& "jdk/internal/misc/VM".equals(owner)
+					&& "getNanoTimeAdjustment".equals(name)
+					&& "(J)J".equals(descriptor))
+			{
+				// replace VM#getNanoTimeAdjustment(long)
+				TimeChangerAgent.log("(" + ccv.className + "#" + methodName + ") target: " + owner + " " + name + " " + descriptor + " opcode=" + opcode + ", isInterface=" + isInterface);
+
+				//-- just call Utility method and return
+				//super.visitMethodInsn(opcode, "ga/d400x/timechanger/util/TimeChangerUtil", "offsetGetNanoTimeAdjustment", descriptor, isInterface);
+				//return;
+				//--
+
+				//-- DEBUG: output long value on top of op stack --//
+				//printLongValOnOpStackTop();
+				//-- DEBUG end --//
+
+				// long offsetInMillis = arg:offsetInSeconds *= -1000L
+				visitLdcInsn(Long.valueOf(-1000L));
+				visitInsn(LMUL);
+
+				// long offsetTimeMillis = Math.addExact(System.currentTimeMillis, Long.valueOf(System.getProperty("HOGE","0")).longValue())
+				createOffsetTimeMillisCode();
+
+				// long diffMillis = Math.addExact(offsetTimeMillis, offsetInMillis)
+				super.visitMethodInsn(INVOKESTATIC, "java/lang/Math", "addExact", "(JJ)J", false);
+				visitInsn(DUP2);	// keep diffMillis operand for lcmp
+
+				// long diffSec = diffMillis / 1000L
+				visitLdcInsn(Long.valueOf(1000L));
+				visitInsn(LDIV);	// long diffSec = diffMillis / 1000
+
+				// long checkSec == (long)((int)diffSec)
+				visitInsn(DUP2);	// keep diffMillis operand for l2i,i2l
+				visitInsn(L2I);
+				visitInsn(I2L);
+
+				visitInsn(LCMP);
+
+				Label label = new Label();
+				Label end = new Label();
+				visitJumpInsn(IFEQ, label);
+
+				// if(work != 0)
+				visitInsn(POP2);
+				visitLdcInsn(Long.valueOf(-1L));
+				visitJumpInsn(GOTO, end);
+
+				// if(work == 0)
+				visitLabel(label);
+				visitLdcInsn(Long.valueOf(1000000L));
+				visitInsn(LMUL);
+
+				visitLabel(end);
+
+				//-- DEBUG: output long value on top of op stack --//
+				//printLongValOnOpStackTop();
+				//-- DEBUG end --//
+
+				ccv.hasTarget = true;
+			} else {
+				super.visitMethodInsn(opcode, owner, name, descriptor, isInterface);
+			}
 		}
 	}
-
 }
